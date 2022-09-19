5a76525f0e6c4c32b59cd07e770d7129a76219b4
Edoardo Vassallo <edo.vas@protonmail.com>
2022-09-19 15:47:02 +0200

    ADD: New animation method for NPCs

--- NPC/NPC.gd
+++ NPC/NPC.gd
@@ -6,6 +6,10 @@ var _dialogue = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# carica spritesheet
+	$Sprite.texture = load(data.spritesheet_path)
+	# carica dialogo
 	var jsonfile = File.new()
 	jsonfile.open(data.dialogue_path, File.READ)
 	_dialogue = parse_json(jsonfile.get_as_text())  
+	
