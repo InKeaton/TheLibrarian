733f86b42530df21dcc7e11c4774707b6fab5405
Edoardo Vassallo <edo.vas@protonmail.com>
2022-09-29 19:17:40 +0200

    ADD: started work on scalarity of collectables

--- collectable.gd
+++ collectable.gd
@@ -1,5 +1,11 @@
 extends StaticBody2D
 
+export  (String) var sprite_path
+
+# dovrò impostare sprite, dimensione hitbox, segnale da inviare alla distruzione
+func ready():
+	pass
+
 func on_body_collected():
 	self.queue_free()
 	
